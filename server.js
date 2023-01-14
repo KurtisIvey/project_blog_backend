666140a461b92f178873f24df3091a84dee0e2d5
Kitsunebackfire <kwolz92@gmail.com>
2023-01-14 15:00:58 -0600

    modify cors to allow local host for api testing

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const postRouter = require("./routes/post.route");
 const userRouter = require("./routes/user.route");
 
 // middlewares
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
