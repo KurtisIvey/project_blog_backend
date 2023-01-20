8f6c5932b2e3796749cd2a2f544f4a0ca350d182
Kitsunebackfire <kwolz92@gmail.com>
2023-01-20 14:52:37 -0600

    modify cors again

--- server.js
+++ server.js
@@ -16,13 +16,8 @@ const postRouter = require("./routes/post.route");
 const userRouter = require("./routes/user.route");
 
 // middlewares
-/** app.use(
-  cors({
-    credentials: true,
-    origin: "https://kitsunebackfire.github.io/project-blog-client",
-  })
-);*/
-
+/** */
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
